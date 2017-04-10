914a94822a78fc09d0c5e6ce70c92d9c2e4fbe2e
Arkadiusz Rybka <arekrybka@gmail.com>
2017-04-10 22:54:02 +0200

    Dodanie metody getWarrantyCost

--- js/script.js
+++ js/script.js
@@ -2,16 +2,25 @@ function Phone(brand, price, color, screenSize) {
 	this.brand = brand;
 	this.price = price;
 	this.color = color;
-	this.screenSize = screenSize;
 };
-Phone.prototype.printInfo = function() {
-	console.log('Marka telefonu to: ' + this.brand + '. Kolor telefonu to: ' + this.color + '. Telefon kosztuje: ' + this.price + '.');
+
+Phone.prototype = {
+	printInfo: function(cost) {
+		console.log('Marka telefonu to: ' + this.brand + '. Kolor telefonu to: ' + this.color + '. Telefon kosztuje: ' + this.price + ' zł. Koszt przedłużonej gwarancji wynosi: ' + cost + ' zł.');
+	},
+	getWarrantyCost: function() {
+		return Math.round(this.price * 0.1);
+	}
 }
 
 var samsungGalaxyS8 = new Phone('Samsung', 3999, 'Midnight Black');
 var iPhone7s = new Phone('Apple', 3649, 'Onyks');
 var onePlusThree = new Phone ('One Plus', 2289, 'Black');
 
-samsungGalaxyS8.printInfo();
-iPhone7s.printInfo();
-onePlusThree.printInfo();
+var samsungExtraWarrantyCost = samsungGalaxyS8.getWarrantyCost();
+var iphoneExtraWarrantyCost = iPhone7s.getWarrantyCost();
+var oneplusExtraWarrantyCost = onePlusThree.getWarrantyCost();
+
+samsungGalaxyS8.printInfo(samsungExtraWarrantyCost);
+iPhone7s.printInfo(iphoneExtraWarrantyCost);
+onePlusThree.printInfo(oneplusExtraWarrantyCost);
