fd674b8dc046681392bc03256981cd32bff2d724
Arkadiusz Rybka <arekrybka@gmail.com>
2017-04-11 15:29:50 +0200

    Poprawki w metodach

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-function Phone(brand, price, color, screenSize) {
+function Phone(brand, price, color) {
 	this.brand = brand;
 	this.price = price;
 	this.color = color;
 };
 
 Phone.prototype = {
-	printInfo: function(cost) {
-		console.log('Marka telefonu to: ' + this.brand + '. Kolor telefonu to: ' + this.color + '. Telefon kosztuje: ' + this.price + ' zł. Koszt przedłużonej gwarancji wynosi: ' + cost + ' zł.');
+	printInfo: function() {
+		console.log('Marka telefonu to: ' + this.brand + '. Kolor telefonu to: ' + this.color + '. Telefon kosztuje: ' + this.price + ' zł. Koszt przedłużonej gwarancji wynosi: ' + this.getWarrantyCost() + ' zł.');
 	},
 	getWarrantyCost: function() {
 		return Math.round(this.price * 0.1);
@@ -17,10 +17,6 @@ var samsungGalaxyS8 = new Phone('Samsung', 3999, 'Midnight Black');
 var iPhone7s = new Phone('Apple', 3649, 'Onyks');
 var onePlusThree = new Phone ('One Plus', 2289, 'Black');
 
-var samsungExtraWarrantyCost = samsungGalaxyS8.getWarrantyCost();
-var iphoneExtraWarrantyCost = iPhone7s.getWarrantyCost();
-var oneplusExtraWarrantyCost = onePlusThree.getWarrantyCost();
-
-samsungGalaxyS8.printInfo(samsungExtraWarrantyCost);
-iPhone7s.printInfo(iphoneExtraWarrantyCost);
-onePlusThree.printInfo(oneplusExtraWarrantyCost);
+samsungGalaxyS8.printInfo();
+iPhone7s.printInfo();
+onePlusThree.printInfo();
